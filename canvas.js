3ee8fe192f7165b50c7e6f2661cc3e37aa86ba21
Jacob Barca <jacob.barca1@hotmail.com>
2019-11-15 15:00:50 +1100

    create canvas file for drawing and animations

---
+++ canvas.js
@@ -0,0 +1,12 @@
+/*  Canvas to visualise the algorithms
+    Author: Jacob Barca
+    Since: 15/11/2019
+    Last Modified: 15/11/2019
+*/
+
+var canvas = document.getElementById("visualiser");
+
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#FF0000";
+//ctx.fillRect(0, 0, 813, 802);
