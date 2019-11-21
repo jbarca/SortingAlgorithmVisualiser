60ccc10864b58a5d8a78e8c70dd5981f6c9cbd24
Jacob Barca <jacob.barca1@hotmail.com>
2019-11-21 15:53:51 +1100

    Added sorting animation for selection sort

--- canvas.js
+++ canvas.js
@@ -9,6 +9,7 @@ import { getAlgorithm } from './algorithms.js';
 /* Constants */
 const MAX_WIDTH = 500;
 const MAX_HEIGHT = 500;
+var sortingSpeed = 1000; // ms
 var array = [1, 2, 3, 4, 5];
 var rects = [];
 var canvas = null;
@@ -17,6 +18,12 @@ var MAX_ELEMENT_HEIGHT = MAX_HEIGHT / Math.max(...array);
 
 var sortButton = document.getElementById("sort");
 var newArrayButton = document.getElementById("new");
+
+function sleep(ms) {
+    /* Function to allow the program to be delayed for a 
+    certain amount of time */
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 sortButton.onclick = function() {
     animateSorting();
@@ -146,6 +153,12 @@ function animateSorting() {
     }
 }
 
+function changeColour(index, colour) {
+    clearRectangles();
+    rects[index].setColour(colour);
+    drawRectangles();
+}
+
 function swap(_array, i, j) {
     var tmp = _array[i];
     _array[i] = _array[j];
@@ -159,21 +172,28 @@ function swapRectangles(_array, i, j) {
     swap(rects, i, j);
 }
 
-function selection_sort() {
+async function selection_sort() {
     var min_index = 0;
     for (var i = 0; i < array.length; i++) {
         min_index = i;
         for (var j = i + 1; j < array.length; j++) {
+            changeColour(j, "#00FF00") // green
+            await sleep(sortingSpeed);
             if (array[j] < array[min_index]) {
+                changeColour(j, "#0000FF") // blue
+                await sleep(sortingSpeed);
                 min_index = j;
             }
+            else {
+                changeColour(j, "#FF0000") // red
+            }
         }
+        changeColour(min_index, "#FF0000");
         swap(array, i, min_index);
         
-        // TODO: Slow down sorting and display it frame by
-        // frame with different colours. 
         clearRectangles();
         swapRectangles(rects, i, min_index);
         drawRectangles();
+        await sleep(sortingSpeed);
     }
 }
